c5ce725c8049f03d2d4604e22646355fa17ecdc3
Evan Glaser <ejglaser@ares.andrew.local.cmu.edu>
2019-04-29 17:37:10 -0400

    can properly read in spiketimes now

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -1,16 +1,16 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _18860_archive_1.so
-_18860_archive_1.so : archive.20/_18860_archive_1.a
+ARCHIVE_OBJS += _32130_archive_1.so
+_32130_archive_1.so : archive.22/_32130_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_18860_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_32130_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_18860_archive_1.so $@
+	@ln -sf .//../simv.daidir//_32130_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.20/_prev_archive_1.a
+_prev_archive_1.so : archive.22/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
